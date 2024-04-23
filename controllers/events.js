4b04512d03428df16aa0efec513fc0a8c0bed74d
jasonunderhill5 <jasonunderhill4005@gmail.com>
2024-04-23 06:56:11 -0700

    implement functionality to create an event post

--- controllers/events.js
+++ controllers/events.js
@@ -4,9 +4,22 @@ import { Event } from "../models/event.js"
     res.render('events/new', {
       title: 'Add Event'
     })
-    
+  }
+
+  function create(req, res) {
+    req.body.author = req.user.profile._id
+    Event.create(req.body)
+    .then(post => {
+      res.redirect('/post')
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
   }
 
 export {
-  newEvent as new
+  newEvent as new,
+  create
+
 }
--- routes/events.js
+++ routes/events.js
@@ -5,6 +5,7 @@ import * as eventsCtrl from '../controllers/events.js'
 const router = Router()
 
 router.get('/new', isLoggedIn, eventsCtrl.new)
+router.post('/', isLoggedIn, eventsCtrl.create)
 
 
 
